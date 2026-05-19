56f3fc0288a537bae0dd8f6c3cf5670cd440b78e
hannanfeisal80-tech <hannanfeisal80@gmail.com>
2026-05-19 18:47:56 +0300

    added new files

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 console.log('Test 123');
 console.log('Welcome to my Nodejs server!');
+
+const http = require('http');
+
+const server = http.createServer((req,res) =>{
+  res.writeHead(200, {'Content-Type': 'text/plain'})
+  res.end('Hello server!')
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+});
